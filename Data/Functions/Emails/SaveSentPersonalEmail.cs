7e832a0d213c60687a81b2b396411f31e4bd0be3
Stefke Velja <stef.fac150@gmail.com>
2023-08-05 04:09:07 +0200

    Changing SaveSentPersonalEmail functional class

--- Data/Functions/Emails/SaveSentPersonalEmail.cs
+++ Data/Functions/Emails/SaveSentPersonalEmail.cs
@@ -16,6 +16,9 @@ namespace honey_beer_server_app.Data.Functions.Emails
             bool sent = false;
             try
             {
+                if (!_context.PersonalEmail.Any(e => e.Email.Equals(email.Email)))
+                    _context.PersonalEmail.Add(email.PersonalEmailInstance);
+                email.PersonalEmailInstance = null;
                 _context.SentPersonalEmail.Add(email);
                 _context.SaveChanges();
                 transaction.Commit();
