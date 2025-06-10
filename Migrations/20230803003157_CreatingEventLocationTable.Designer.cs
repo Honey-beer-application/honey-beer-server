2f392b64265fbc3ac6ce802ebb18c849cfa50561
Stefan Veljkovic <stef.fac150@gmail.com>
2025-06-10 07:41:38 +0200

    Relocated DBBrocker and DBContext classes

--- Migrations/20230803003157_CreatingEventLocationTable.Designer.cs
+++ Migrations/20230803003157_CreatingEventLocationTable.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using honey_beer_server_app.DBBrockers;
+using honey_beer_server_app.Repositories.DBContextNamespace;
 
 #nullable disable
 
